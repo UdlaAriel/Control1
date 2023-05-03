0800902509ee9df9c62d8d0255cd85696ffcec8a
UdlaAriel <laboratorio@udla-ec.int>
2023-05-03 15:14:59 -0500

    Creación de bucles que definirán el número ingresado en varios dígitos

--- Control1.c
+++ Control1.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 
+/**/
 int main ()
 {
     int num_entrada;
     int vflag  = 1;
+    int digit[50];
 
     /*Ingreso de datos*/
     scanf("%d", &num_entrada);
-    int *pointer_to_num_entrada = num_entrada;
-
-    /*
+    
     do
     {
+        for (int i = 0; i < 50; i++)
+        {
+            digit[i]= num_entrada % 10; 
+            num_entrada /= 10; 
+            
+            printf("%d", digit[i]);
 
+            if(num_entrada >= 0)
+            {
+                vflag = 0;
+                break;
+            }
+        }
+        
     }
     while (vflag != 0);
-    */
-   pointer_to_num_entrada++;
 
-   printf("%d", *pointer_to_num_entrada);
+	
+    printf("%d", num_entrada);
 
     return 0;
 }
